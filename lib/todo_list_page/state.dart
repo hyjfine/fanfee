20d8142de6a84768500ed51208362eff8dda5cc2
heyongjian <heyongjian@kezaihui.com>
2019-07-21 17:16:17 +0800

    base

---
+++ lib/todo_list_page/state.dart
@@ -0,0 +1,42 @@
+import 'package:fish_redux/fish_redux.dart';
+
+import 'report_component/component.dart';
+import 'todo_component/component.dart';
+
+class PageState implements Cloneable<PageState> {
+  List<ToDoState> toDos;
+
+  @override
+  PageState clone() {
+    return PageState()..toDos = toDos;
+  }
+}
+
+PageState initState(Map<String, dynamic> args) {
+  //just demo, do nothing here...
+  return PageState();
+}
+
+class ReportConnector extends Reselect2<PageState, ReportState, int, int> {
+  @override
+  ReportState computed(int sub0, int sub1) {
+    return ReportState()
+      ..done = sub0
+      ..total = sub1;
+  }
+
+  @override
+  int getSub0(PageState state) {
+    return state.toDos.where((ToDoState tds) => tds.isDone).toList().length;
+  }
+
+  @override
+  int getSub1(PageState state) {
+    return state.toDos.length;
+  }
+
+  @override
+  void set(PageState state, ReportState subState) {
+    throw Exception('Unexcepted to set PageState from ReportState');
+  }
+}
