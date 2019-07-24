a0441ac845d3684161f7142b3e4368b1fb145a76
heyongjian <heyongjian@kezaihui.com>
2019-07-24 22:24:39 +0800

    with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,114 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart' hide Action;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'todo_edit_page/page.dart';
 import 'todo_list_page/page.dart';
 
-void main() => runApp(createApp());
+void main() => runApp(MyApp1());
+
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Baby Names',
+      home: MyHomePage1(),
+    );
+  }
+}
+
+class MyHomePage1 extends StatefulWidget {
+  @override
+  _MyHomePageState1 createState() {
+    return _MyHomePageState1();
+  }
+}
+
+class _MyHomePageState1 extends State<MyHomePage1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Baby Name Votes')),
+      body: _buildBody(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          Firestore.instance
+              .collection('baby')
+              .document('test')
+              .setData({'name': 'add', "votes": 5})
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trai
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    // TODO: get actual snapshot from Cloud Firestore
+
+//    return _buildList(context, dummySnapshot);
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('baby').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => record.reference.updateData({'votes': record.votes + 1}),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$votes>";
+}
 
 Widget createApp() {
   final AbstractRoutes routes = PageRoutes(
